0302df54f5b411c11c8cb468ea5043e540928081
fredrik jacobson <fredrik.jacobson@annotell.com>
2022-12-12 20:22:48 +0100

    breadth first

---
+++ src/bin/day12.rs
@@ -0,0 +1,5 @@
+use aoc_2022::day12::day12_pt_1;
+
+pub fn main() {
+    day12_pt_1();
+}
--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashSet, hash::Hash, str::FromStr};
+use std::{
+    collections::{HashSet, VecDeque},
+    hash::Hash,
+    str::FromStr,
+};
 
 use crate::helper::{read_lines, ParseError};
 
@@ -12,188 +16,124 @@ struct Position {
 
 #[test]
 pub fn test_day12_pt_1() {
-    let lines: Vec<String> = read_lines(12, false);
-    let grid: Grid = lines.iter().map(|l| l.chars().collect()).collect();
-    let start = find_start(&grid);
-    let destination = find_elves(&grid);
-
-    let steps = walk(
-        HashSet::new(),
-        &grid,
-        &Position {
-            x: start.0 as i32,
-            y: start.1 as i32,
-        },
-        'a',
-        0,
-    );
-
-    println!("Part1 score is {:?}", steps);
+    day12_pt_1();
     assert!(false);
 }
 
-#[derive(Clone, Copy, Debug)]
-enum Direction {
-    Up,
-    Right,
-    Down,
-    Left,
-}
-use Direction::*;
-
-#[derive(Clone, Copy)]
-enum Elevation {
-    Next,
-    Current,
+pub fn day12_pt_1() {
+    let lines: Vec<String> = read_lines(12, false);
+    let (grid, start, destination) = extract_grid(lines);
+    let steps = shortest_path(&grid, start, &destination).unwrap();
+    println!("Part1 score is {:?}", steps);
 }
 
-use Elevation::*;
+fn extract_grid(lines: Vec<String>) -> (Vec<Vec<char>>, Position, Position) {
+    let mut grid: Grid = lines.iter().map(|l| l.chars().collect()).collect();
+    let start = find_start(&grid);
+    let destination = find_elves(&grid);
+    grid[start.y as usize][start.x as usize] = 'a';
+    grid[destination.y as usize][destination.x as usize] = 'z';
+    (grid, start, destination)
+}
 
-fn walk(
-    visited: HashSet<Position>,
-    grid: &Grid,
-    position: &Position,
-    elevation: char,
-    steps: usize,
-) -> Option<usize> {
-    let next_elevation: char = if elevation == 'z' {
-        'E'
-    } else {
-        char::from_u32(elevation as u32 + 1).unwrap()
-    };
+fn shortest_path(grid: &Grid, start: Position, destination: &Position) -> Option<i32> {
+    println!("Start: {:?}", start);
+    println!("Elves: {:?}", destination);
 
-    if elevation == 'E' {
-        return Some(steps);
-    }
-
-    let mut searches = Vec::new();
-    for direction in vec![Up, Right, Down, Left] {
-        for search_elevation in vec![Next, Current] {
-            searches.push((direction, search_elevation))
+    let mut queue = VecDeque::new();
+    queue.push_back(((start.x as usize, start.y as usize), 0));
+    let mut visited = HashSet::<Position>::new();
+    while let Some(((x, y), len)) = queue.pop_front() {
+        if (y, x) == (destination.y as usize, destination.x as usize) {
+            return Some(len);
         }
-    }
+        for (dx, dy) in [(0, -1), (-1, 0), (0, 1), (1, 0)] {
+            let (nx, ny) = ((x as isize + dx) as usize, (y as isize + dy) as usize);
+            if let Some(&square) = grid.get(ny).and_then(|row| row.get(nx)) {
+                if grid[y][x] as u32 + 1 >= square as u32
+                    && !visited.contains(&Position {
+                        x: nx as i32,
+                        y: ny as i32,
+                    })
+                {
+                    visited.insert(Position {
+                        x: nx as i32,
+                        y: ny as i32,
+                    });
 
-    if steps > 700 {
-        for row in 0..grid.len() {
-            let mut line = " ".repeat(grid[0].len());
-            for col in 0..grid[0].len() {
-                if visited.contains(&Position {
-                    x: col as i32,
-                    y: row as i32,
-                }) {
-                    line.insert(col, '#');
+                    queue.push_back(((nx, ny), len + 1));
                 }
             }
-            println!("Row: {:02} {}", row, line);
         }
-        println!("Elevation: {}", elevation);
-        return None;
     }
-    let mut branches: Vec<usize> = Vec::new();
+    None
+}
 
-    for (direction, search_elevation) in searches {
-        let needle = match search_elevation {
-            Next => next_elevation,
-            Current => elevation,
-        };
-        let (x_delta, y_delta) = match direction {
-            Up => (0, -1),
-            Right => (1, 0),
-            Down => (0, 1),
-            Left => (-1, 0),
-        };
-
-        if let Some(new_elevation) =
-            is_elevation_match(&visited, grid, &position, x_delta, y_delta, needle)
-        {
-            println!(
-                "Step: {} Elevation {} at position {:?} - walking to {:?} elevation {}",
-                steps, elevation, position, direction, new_elevation
-            );
-            let mut new_visited = visited.clone();
-            new_visited.insert(position.clone());
-            if let Some(in_steps) = walk(
-                new_visited,
-                grid,
-                &Position {
-                    x: position.x + x_delta,
-                    y: position.y + y_delta,
-                },
-                new_elevation,
-                steps + 1,
-            ) {
-                branches.push(in_steps);
+fn print_grid(grid: &Vec<Vec<char>>, visited: &HashSet<Position>, elevation: char) {
+    for row in 0..grid.len() {
+        let mut line = " ".repeat(grid[0].len());
+        for col in 0..grid[0].len() {
+            if visited.contains(&Position {
+                x: col as i32,
+                y: row as i32,
+            }) {
+                line.insert(col, '#');
             }
         }
+        println!("Row: {:02} {}", row, line);
     }
-
-    if branches.is_empty() {
-        println!(
-            "Step: {} Elevation {} at position {:?} - No match found for {}",
-            steps, elevation, position, next_elevation
-        );
-        None
-    } else {
-        println!("OK Branches: {:?}", branches);
-        branches.sort();
-        Some(branches[0])
-    }
+    println!("Elevation: {}", elevation);
 }
 
-fn is_elevation_match(
-    visited: &HashSet<Position>,
-    grid: &Grid,
-    position: &Position,
-    x_delta: i32,
-    y_delta: i32,
-    needle: char,
-) -> Option<char> {
-    let x = position.x + x_delta;
-    let y = position.y + y_delta;
-    let num_rows = grid.len();
-    let num_cols = grid[0].len();
-
-    if visited.contains(&Position { x, y }) {
-        None
-    } else if x < 0 || y < 0 || x as usize >= num_cols || y as usize >= num_rows {
-        None
-    } else {
-        let x = x as usize;
-        let y = y as usize;
-
-        if grid[y][x] == needle {
-            Some(needle)
-        } else {
-            None
-        }
-    }
-}
-
-fn find_start(grid: &Grid) -> (usize, usize) {
+fn find_start(grid: &Grid) -> Position {
     for col in 0..grid[0].len() {
         for row in 0..grid.len() {
             if grid[row][col] == 'S' {
-                return (col, row);
+                return Position {
+                    x: col as i32,
+                    y: row as i32,
+                };
             }
         }
     }
-    return (0, 0);
+    return Position { x: 0, y: 0 };
 }
 
-fn find_elves(grid: &Grid) -> (usize, usize) {
+fn find_elves(grid: &Grid) -> Position {
     for col in 0..grid[0].len() {
         for row in 0..grid.len() {
             if grid[row][col] == 'E' {
-                return (row, col);
+                return Position {
+                    x: col as i32,
+                    y: row as i32,
+                };
             }
         }
     }
-    return (0, 0);
+    return Position { x: 0, y: 0 };
 }
 
 #[test]
 pub fn test_day12_pt_2() {
     let lines: Vec<String> = read_lines(12, false);
-    println!("Part2 score is {:?}", 0);
+    let (grid, start, destination) = extract_grid(lines);
+
+    let mut shortest = 10000;
+    for start_y in 0..grid.len() {
+        let steps = shortest_path(
+            &grid,
+            Position {
+                x: 0,
+                y: start_y as i32,
+            },
+            &destination,
+        )
+        .unwrap();
+        if steps < shortest {
+            shortest = steps;
+        }
+    }
+
+    println!("Part2 score is {:?}", shortest);
     assert!(false);
 }
