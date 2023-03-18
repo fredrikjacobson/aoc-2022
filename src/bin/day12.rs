d869cc117291fb034cdb31bb8be5f8bca3f54e59
fredrik jacobson <fredrik.jacobson@annotell.com>
2023-03-18 14:14:07 +0100

    day 12-15

--- src/bin/day12.rs
+++
@@ -1,5 +0,0 @@
-use aoc_2022::day12::day12_pt_1;
-
-pub fn main() {
-    day12_pt_1();
-}
--- src/bin/day15.rs
+++
@@ -1,6 +0,0 @@
-use aoc_2022::{day12::day12_pt_1, day15::day15_pt_2};
-
-pub fn main() {
-    let answer = day15_pt_2();
-    println!("{:?}", answer);
-}
